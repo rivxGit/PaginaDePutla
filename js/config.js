875a16a8b315075a2298ea7f1f37aa5beb7831cd
rivxGit <jrivera2223@oax.conalep.edu.mx>
2026-06-10 11:29:47 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
 // 1. Configura las credenciales reales de tu Supabase
-const SUPABASE_URL = "https://svnlwqzdfmiolxzbjnqb.supabase.co";
+const SUPABASE_URL = "https://svnlwqzdfmiolxzbjnqb.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2bmx3cXpkZm1pb2x4emJqbnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3Nzc1ODgsImV4cCI6MjA5NjM1MzU4OH0.xGmge2VPqKK96RcYKiZMQYN0MVKaH-bf7nZJuaeKDQw"; 
 
 // 2. Inicializa la conexión global con la base de datos y almacén
