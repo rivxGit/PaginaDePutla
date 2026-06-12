ed00bb11f08c58d05cdddef30689a28039450cae
rivxGit <jrivera2223@oax.conalep.edu.mx>
2026-06-12 13:11:33 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
-// 1. Configura las credenciales reales de tu Supabase
-const SUPABASE_URL = "https://svnlwqzdfmiolxzbjnqb.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdWJhYmFzZSIsInJlZiI6InN2bmx3cXpkZm1pb2x4emJqbnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2M2F1b2I0MH0.xGmge2VPqKK96RcYKjZMQYN";
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
 
-// 2. Inicializa la conexión global usando la 's' minúscula oficial
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    <script src="js/galeria-dinamica.js"></script>
+</body>
